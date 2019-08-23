00aff8139bd0cdc77830fe48dad2a6457171adaa
aquiles <jjoseaquiless@gmail.com>
2019-08-23 15:55:21 -0300

    first_commit

---
+++ darker.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector <pair<int,int>> cord = {
+    {0,0},{0,1},{1,0},{1,1},
+    {-1,-1},{-1,0},{0,-1}
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    size_t h,w;
+    cin >> h >> w; 
+    vector <bool> adjMatrix(h,w);
+    return 0;
+}
