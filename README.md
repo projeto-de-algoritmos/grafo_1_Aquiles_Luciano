1ccd7b6470517242c7b4abd9e0fcd9ae185e319b
aquiles <jjoseaquiless@gmail.com>
2019-08-25 01:41:45 -0300

    ac result

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## Objetivo
 
 Implementação com resultado AC no problema  [Darker and Darker](https://atcoder.jp/contests/agc033/tasks/agc033_a) da plataforma de competições AtCoder
+
+[Resultado no AtCoder](https://atcoder.jp/contests/agc033/submissions/7130237)
--- darker.cpp
+++ darker.cpp
@@ -4,49 +4,46 @@ const long long MAX {1005};
 
 vector <pair<int,int>> adj = { {0,1},{1,0},{-1,0},{0,-1} };
 queue <pair<int,int>> que;
-vector <vector<bool>> visited(MAX);
-vector <vector<int>> dist(MAX);
+vector <vector<bool>> visited(MAX,vector<bool>(MAX));
+vector <vector<int>> dist(MAX,vector<int>(MAX));
 
-void bfs(size_t h,size_t w)
+pair <int,int> bfs(int h,int w)
 {
+    pair <int,int> u;
     //the queue is alread populated with the nodes we need to start
     dist[que.front().first][que.front().second] = 0;
     while (not que.empty())
     {
-        auto u = que.front();
+        u = que.front();
         que.pop();
         for (const auto& v : adj)
         {
-            cout << u.first + v.first << u.second + v.second;
-            if (visited[u.first + v.first][u.second + v.second] 
-                or u.first + v.first > h-1
+            if (u.first + v.first > h-1
                 or u.first + v.first < 0
                 or u.second + v.second > w-1
                 or u.second + v.second < 0 )
                 continue;
+            
+            if (visited[u.first + v.first][u.second + v.second])
+                continue;
 
             visited[u.first + v.first][u.second + v.second] = true;
-            dist[v.first][v.second] = dist[u.first][u.second] + 1;
-            que.push(v);
+            dist[u.first + v.first][u.second + v.second] = dist[u.first][u.second] + 1;
+            que.push({u.first + v.first,u.second + v.second});
         }
     }
+    return u;
 }
 
 int main(){
     // turn off the sinc with stdio.h to make the program faster
-    //ios::sync_with_stdio(false);
-    size_t h,w;
+    ios::sync_with_stdio(false);
+    int h,w;
     char dot;
     cin >> h >> w; 
-    
-    for (int i = 0; i < h; i++) { 
-        // declare  the i-th row to size of column 
-        visited[i] = vector<bool>(w);
-        dist[i] =  vector<int>(w);
-    }
 
-    for(size_t i = 0; i<h; i++){
-        for(size_t j = 0; j<w; j++){
+    for(int i = 0; i<h; i++){
+        for(int j = 0; j<w; j++){
             cin >> dot;
             // mark the node as unvisited
             if(dot == '.') visited[i][j] = false;
@@ -58,10 +55,10 @@ int main(){
             }
         }
     }
-    
-    bfs(h,w);
+    //capture the distance betwen the first read node and the last 
+    auto res = bfs(h,w);
 
-    cout << dist[h-1][w-1] << endl;
+    cout << dist[res.first][res.second] << endl;
 
     return 0;
 }
